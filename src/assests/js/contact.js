66e309b628ef229c6c2ab90cbacd0d5222246143
Vishesh-04 <visheshjindal04@gmail.com>
2025-01-30 14:17:45 +0530

    Thirteen Commit

---
+++ src/assests/js/contact.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("appsheet-form");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+
+    fetch(
+      "https://script.google.com/macros/s/AKfycbzt5ZMmkUYiKPVv6lGsF9e6rSAX_Z-eWiX9vyOmca9KeHhvgoTgApZeswnlEsk9Zys7cA/exec",
+      {
+        method: "POST",
+        body: formData,
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        alert("Registration successful! Your form has been submitted.");
+        form.reset();
+      })
+      .catch((error) => {
+        console.error("Error submitting form:", error);
+        alert("Failed to submit form. Please try again.");
+      });
+  });
+});
